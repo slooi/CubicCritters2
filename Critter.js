433b9caaeaceb4f623dc1b19b51b2d8aad214835
slooi <s.looi2@outlook.com>
2020-04-20 03:27:08 +1200

    Fix inputs and can eat now

--- Critter.js
+++ Critter.js
@@ -8,7 +8,7 @@ class Critter{
 		this.dir = dir
 		this.speed = speed
 		this.w = 16
-		this.hp = 250+~~(Math.random()*250)
+		this.hp = 500+~~(Math.random()*250)
 		if(nn === undefined){
 			this.nn = new NeuralNetwork()
 			this.nn.build([16,10,10,1])
@@ -21,6 +21,9 @@ class Critter{
 	}
 	update(i){
 		this.hp--
+		if(Math.abs(this.dir)>Math.PI){
+			throw new Error('ERROR: this shouldt happen!')
+		}
 		if(this.deleted===true){
 			throw new Error('ERROR this shouldnt happen!')
 		}
@@ -32,14 +35,14 @@ class Critter{
 		}else{
 			this.inputs = []
 			const numNeighbours = 4
-			const turnSpeed = Math.PI*0.01
+			const turnSpeed = Math.PI*0.2
 
 			grid.rangeQueryInit(150,numNeighbours,this)
 			let neighbours = grid.foundNeighbours
 			let neighboursFood = grid.foundNeighboursFood
 
 			// !@#!@#!@# neighbour.length are not the same!
-			neighbours.forEach(neighbour=>{magAngline2(this.x,this.y,neighbour.x,neighbour.y,0.35,0.9,0.9)})
+			neighbours.forEach(neighbour=>{magAngline(this.x,this.y,neighbour.x,neighbour.y,0.35,0.9,0.9)})
 			neighboursFood.forEach(food=>{angline(this.x,this.y,food.x,food.y,248.0/255.0,0.75,0.5)})//=238.0/255.0,g=253.0/255.0,b=237.0/255.0
 			this.foodCollisions(neighboursFood)
 
@@ -52,16 +55,30 @@ class Critter{
 			this.preprocess(neighboursFood,numNeighbours)
 			// debugVar = [this,neighbours,this.inputs]
 			if(this.inputs[1].length !==undefined){
-				// console.log(':D')
+				console.log(':D')
 			}
-			console.log(this)
-			console.log(this.inputs)
+			// console.log(this)
+			// console.log(this.inputs)
 			// console.log(this)
 			// console.log(this.inputs)		// !@#!@#!@# CHANGE MY INPUTS THEY ARE ALL SCREWED UP  !!@#!@#!@#!@#!@#!@#!@#
 			const output = this.nn.feedForward(this.inputs)
 			this.dir += 2*turnSpeed*output[0][0]-turnSpeed
+
+			// ASSUMES critters can't turn more than 180 degrees per frame (for if else statement to work )
+			// this.dir = this.dir % Math.PI*2		// this should make assumption always true !@#!@#!@#!@# CAUSES PROBLEMS
+			if(this.dir<-Math.PI){
+				// dir < -180
+				this.dir = Math.PI*2 + this.dir
+			}else if(this.dir>Math.PI){
+				// dir > 180
+				this.dir = -Math.PI*2 + this.dir
+			}		//!@#!@#!@##!@# un tested
+
+
 			this.x += this.speed * Math.cos(this.dir)
 			this.y += this.speed * Math.sin(this.dir)
+
+
 
 			this.render()
 			
@@ -71,14 +88,38 @@ class Critter{
 		for(let i=0;i<numNeighbours*2;i+=2){	// .length NOT numNeighbours, as may not have enough neighbours
 			if(neighbourList[i] === undefined){
 				// dis
-				this.inputs.push(10)
+				this.inputs.push(2)
 				// angle
-				this.inputs.push(10)
+				this.inputs.push(-1)
 			}else{
+				// dis
 				this.inputs.push(  Math.sqrt(  Math.pow(neighbourList[i].x-this.x,2)+Math.pow(neighbourList[i].y-this.y,2)  )/(1+canvas.width)		)	//TIED!@#!@#!@#
-				this.inputs.push( (Math.atan2(neighbourList[i].y-this.y,neighbourList[i].x-this.x)-this.dir)%Math.PI			)//TIED!@#!@#!@#
+				// angle
+				this.inputs.push( this.getAngle(this.dir,Math.atan2(this.y-neighbourList[i].y,this.x-neighbourList[i].x))  /Math.PI )//TIED!@#!@#!@#
+				// console.log(this.getAngle(this.dir,Math.atan2(this.y-neighbourList[i].y,this.x-neighbourList[i].x))  /Math.PI)
 			}
 		}
+	}
+	getAngle(o,t){
+		// returns the difference in angles from 'o'riginal to 't'arget
+		// result should be from -180 to 180 in RADIANS
+		const result = Math.atan2(Math.sin(t-o), Math.cos(t-o))
+		if(Math.abs(result)>Math.PI){
+			throw new Error('ERROR: this shouldnt be happening')
+		}
+		return result
+
+		// if(Math.abs(t)>Math.PI){
+		// 	throw new Error('ERROR: this shouldnt be happening')
+		// }
+		// if(Math.abs(o)>Math.PI){
+		// 	throw new Error('ERROR: this shouldnt be happening')
+		// }
+		// const result = mod(t-o + 180, 360) - 180
+		// if(Math.abs(result)>Math.PI*2){
+		// 	throw new Error('ERROR: this shouldnt be happening')
+		// }
+		return result
 	}
 	render(){
 		square(this.x,this.y,this.w)
@@ -88,7 +129,7 @@ class Critter{
 			const food = neighboursFood[i]
 			if(   (Math.pow(food.x-this.x,2)+Math.pow(food.y-this.y,2))   <Math.pow(this.w*0.6,2)){
 				neighboursFood[i].delete()
-				this.hp+=1000
+				this.hp+=250
 				this.reproduce()
 			}else{
 				// once list is sorted, we know that preceeding food are too far to reach
@@ -98,11 +139,11 @@ class Critter{
 	}
 	reproduce(){
 		// THIS SUDDENLY CREATES NEW CRITTERS  NOT in grid
-		for(let i=0;i<1;i++){
+		for(let i=0;i<4;i++){
 			if(critterList.length<maxPopulation){
 				const a = this.nn.createMutatedCopy(0.05)
 				critterList.push(
-					new Critter(this.x,this.y,0,this.speed, this.nn.createMutatedCopy(0.05))
+					new Critter(this.x,this.y,this.dir,this.speed, this.nn.createMutatedCopy(0.05))
 				)
 			}else{
 				break
